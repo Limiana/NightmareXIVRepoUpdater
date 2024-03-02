36a6e14d43b81c8636916a3146c265a1b57f85c7
Limiana <5073202+Limiana@users.noreply.github.com>
2024-03-02 04:49:12 +0300

    add delay in the beginning

--- NightmareXIVRepoUpdater/Program.cs
+++ NightmareXIVRepoUpdater/Program.cs
@@ -6,6 +6,11 @@ internal class Program
 
     static void Main(string[] args)
     {
+        for (int i = 10; i > 0; i--)
+        {
+            Console.WriteLine($"Counting down, {i}");
+            Thread.Sleep(1000);
+        }
         Updater = new(args);
         var task = Updater.Run();
         task.Wait();
